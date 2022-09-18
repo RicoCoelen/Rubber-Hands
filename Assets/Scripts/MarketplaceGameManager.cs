0eca1607ef0226bd31d037c18b48091208d726a1
DESKTOP-PQ7P7SK\callr <rico.coelen@gmail.com>
2022-09-18 16:36:29 +0200

    Set-up for development

---
+++ Assets/Scripts/MarketplaceGameManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarketplaceGameManager : MonoBehaviour
+{
+    [Header("Game Objects")]
+    [SerializeField] private GameObject enemy;
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject aicrowd;
+    [SerializeField] private GameObject gameball;
+    [SerializeField] private GameObject screens;
+
+    [Header("Game Positions")]
+    [SerializeField] private GameObject aicrowdpos;
+    [SerializeField] private GameObject antagonistpos;
+
+    [Header("Game Variables")]
+    [SerializeField] private float score;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
